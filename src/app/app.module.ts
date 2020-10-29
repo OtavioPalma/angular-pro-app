595855779b04d5b38e18837d6450b85df8477f8a
Otavio Messias Palma <otaviopalma@gmail.com>
2020-10-29 11:34:19 -0300

    Firebase Config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { Store } from 'store';
+
+import { AppComponent } from './containers/app/app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [Store],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+/*
+ var firebaseConfig = {
+    apiKey: "AIzaSyABS37TKYOhrp_q_X3p1y_-bYXCPjMB7T0",
+    authDomain: "fitness-app-otavio.firebaseapp.com",
+    databaseURL: "https://fitness-app-otavio.firebaseio.com",
+    projectId: "fitness-app-otavio",
+    storageBucket: "fitness-app-otavio.appspot.com",
+    messagingSenderId: "97988052475",
+    appId: "1:97988052475:web:6a1f99e885e67c54e7b75e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+*/
---
+++ src/app/containers/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div>Hello Ultimate Angular!</div>`,
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {}
